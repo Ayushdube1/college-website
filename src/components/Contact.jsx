4d8c3edf5cb7104b8a71a88491fc054549c4b13f
Ayushdube1 <ayush.dube48@gmail.com>
2024-10-08 23:50:13 +0530

    web3forms added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,31 @@ import location_icon from '../assets/location-icon.png'
 import white_arrow from '../assets/white-arrow.png'
 
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "ad555043-ea63-4bc3-837f-30f61cf7f74f");
+    
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className ="contact">
         <div className="contact-col">
@@ -19,7 +44,7 @@ const Contact = () => {
             </ul>
         </div>
         <div className="contact-col">
-            <form>
+            <form onSubmit={onSubmit}>
                 <label>Your name</label>
                 <input type="text" name ="name" placeholder="Enter your name here" required />
                 <label>Phone number</label>
@@ -28,10 +53,10 @@ const Contact = () => {
                 <label>Your email</label>
                 <input type="email"  name ="email" placeholder="Enter your mail here" required />
                 <label>Write your message here</label>
-                <textarea name ="message" type="text" rows="6" placeholder="Enter your message here" required></textarea>
+                <textarea name ="message"  rows="6" placeholder="Enter your message here" required></textarea>
                 <button type ="submit" className="btn dark-btn">Submit now <img src ={white_arrow}/></button>
             </form>
-            <span></span>
+            <span>{result}</span>
         </div>
       
     </div>
