8338c34c66f2874ae5b313a5ca86b6ba1cc8ba78
Ayushdube1 <ayush.dube48@gmail.com>
2024-10-08 17:58:18 +0530

    title head prepared

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Title from './components/Title'
 
 const App = () => {
   return (
     <>
     <Navbar/>
      <Hero/>
-     
+     <div className="container">
+      <Title subtitle="Our pragram" title="what we offer" />
+     </div>
     </>
   )
 }
